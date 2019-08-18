2cdd722c433d094989f28329cebd701b380ebcd5
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2019-08-18 18:47:48 +0300

    Install socket.io in the backend

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const io = require('socket.io')().listen(8000);
 
 io.on('connection', (socket) => {
-  console.log('Connected.');
+  socket.on('login', (credentials) => {
+    console.log('Login event was sent.');
+    if (credentials.username === 'joo' && credentials.password === 'jaa') {
+      console.log('Login success.');
+      socket.emit('loginSuccess');
+    } else {
+      console.log('Login failure.');
+      socket.emit('loginFailure');
+    }
+  });
 });
